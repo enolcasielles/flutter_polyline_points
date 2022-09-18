706ad9e16125b1a1376dc0665760d039161a690c
Enol Casielles <enolcasielles@gmail.com>
2022-09-18 11:16:35 +0200

    change decodeEncodedPolyline method to work with flutter web

--- lib/src/network_util.dart
+++ lib/src/network_util.dart
@@ -66,33 +66,47 @@ class NetworkUtil {
   /// for more info about the algorithm check https://developers.google.com/maps/documentation/utilities/polylinealgorithm
   ///
   ///return [List]
-  List<PointLatLng> decodeEncodedPolyline(String encoded) {
+List<PointLatLng> decodeEncodedPolyline(String encoded) {
     List<PointLatLng> poly = [];
     int index = 0, len = encoded.length;
     int lat = 0, lng = 0;
+    BigInt Big0 = BigInt.from(0);
+    BigInt Big0x1f = BigInt.from(0x1f);
+    BigInt Big0x20 = BigInt.from(0x20);
 
     while (index < len) {
-      int b, shift = 0, result = 0;
+      int shift = 0;
+      BigInt b, result;
+      result = Big0;
       do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
+        b = BigInt.from(encoded.codeUnitAt(index++) - 63);
+        result |= (b & Big0x1f) << shift;
         shift += 5;
-      } while (b >= 0x20);
-      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      } while (b >= Big0x20);
+      BigInt rshifted = result >> 1;
+      int dlat;
+      if (result.isOdd)
+        dlat = (~rshifted).toInt();
+      else
+        dlat = rshifted.toInt();
       lat += dlat;
 
       shift = 0;
-      result = 0;
+      result = Big0;
       do {
-        b = encoded.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
+        b = BigInt.from(encoded.codeUnitAt(index++) - 63);
+        result |= (b & Big0x1f) << shift;
         shift += 5;
-      } while (b >= 0x20);
-      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      } while (b >= Big0x20);
+      rshifted = result >> 1;
+      int dlng;
+      if (result.isOdd)
+        dlng = (~rshifted).toInt();
+      else
+        dlng = rshifted.toInt();
       lng += dlng;
-      PointLatLng p =
-          new PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble());
-      poly.add(p);
+
+      poly.add(PointLatLng((lat / 1E5).toDouble(), (lng / 1E5).toDouble()));
     }
     return poly;
   }
